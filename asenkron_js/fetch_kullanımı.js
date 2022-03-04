67bd125d86d3d7c38e482386e67741240d956210
selmanjir <cselmanjir@gmail.com>
2022-03-04 12:56:50 +0300

    Continue Education 13

--- asenkron_js/fetch_kullanımı.js
+++ asenkron_js/fetch_kullanımı.js
@@ -1,8 +1,12 @@
 const btntextGetir = document.getElementById('btn-text-getir');
 const btnJsonGetir = document.getElementById('btn-json-getir');
+const btnApidenJSONGetir = document.getElementById('btn-api-json-getir')
+const btnJSONVeriEkle = document.getElementById('btn-json-veri-ekle');
 
 btntextGetir.addEventListener('click',getText);
 btnJsonGetir.addEventListener('click',getJSON);
+btnApidenJSONGetir.addEventListener('click',getJSONFromApi);
+btnJSONVeriEkle.addEventListener('click',jsonVeriEkle);
 
 const sonucDiv = document.getElementById('sonuc');
 
@@ -22,7 +26,7 @@ fetch('ogrenciler.json')
     sonucDiv.innerText =cikti;
 });
 
-}
+};
 
 function getText(e) {
      e.preventDefault();
@@ -40,3 +44,51 @@ function getText(e) {
     .then(sonuc => sonucDiv.innerText=sonuc)
     .catch(hata => console.log(hata)); 
  };
+
+function getJSONFromApi(e) {
+    e.preventDefault();
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(sonuc => ekranaYazdir(sonuc)); 
+}
+
+
+function ekranaYazdir(data) {
+    let cikti = '';
+    data.map(user => {
+        cikti += `<li>${user.name}</li>`
+    });
+    sonucDiv.innerHTML = cikti;
+}
+
+// function jsonVeriEkle(e) {
+//     e.preventDefault();
+
+//     fetch('https://jsonplaceholder.typicode.com/posts',{
+//         method: 'POST',
+//         body: JSON.stringify({
+//             title: 'deneme',
+//             body: 'body alanı',
+//             userId: 5
+//          }),
+//         headers: {'Content-type':'application/json'}
+//     })
+//         .then(response => response.json())
+//         .then(sonuc => console.log(sonuc))
+// } 
+async function jsonVeriEkle(e) {
+    e.preventDefault();
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'deneme',
+            body: 'body alanı',
+            userId: 5
+         }),
+        headers: {'Content-type':'application/json'}
+    })
+    const sonuc = await response.json();
+    console.log(sonuc);
+} 
